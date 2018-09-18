6402164ce0213b78e367d556c786fc76b5832447
Гарипов Дамир <GaripovD@mail.ru>
2018-09-19 00:21:13 +0500

    Create Nuget Package

---
+++ DataBus/Build.ps1
@@ -0,0 +1,7 @@
+﻿#[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("cp866")
+
+$vs_instance = Get-VSSetupInstance
+$buildPath = $vs_instance.InstallationPath + "\MSBuild\15.0\bin\MSBuild.exe"
+& $buildPath 'c:\Users\Domer\source\repos\ExampleProjects\MassTransit_Saga\DataBus\DataBus.csproj' '/p:Configuration=Debug'
+& $buildPath 'c:\Users\Domer\source\repos\ExampleProjects\MassTransit_Saga\DataBus\DataBus.csproj' '/p:Configuration=Debug_net461'
+nuget pack .\nuget_package\DataBus.nuspec -OutputDirectory .\nuget_package
